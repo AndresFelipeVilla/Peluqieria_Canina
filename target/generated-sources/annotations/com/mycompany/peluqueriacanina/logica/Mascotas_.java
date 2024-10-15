7c227dc481e61c9192b6581ed3429cd7f55567d3
Felipe villa <andresfelipevillapardo7@gmail.com>
2024-10-14 20:20:00 -0500

    Se sube una nueva version

--- target/generated-sources/annotations/com/mycompany/peluqueriacanina/logica/Mascotas_.java
+++ target/generated-sources/annotations/com/mycompany/peluqueriacanina/logica/Mascotas_.java
@@ -5,7 +5,7 @@ import javax.annotation.processing.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-10-02T22:14:49", comments="EclipseLink-2.7.12.v20230209-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-10-14T12:10:51", comments="EclipseLink-2.7.12.v20230209-rNA")
 @StaticMetamodel(Mascotas.class)
 public class Mascotas_ { 
 
